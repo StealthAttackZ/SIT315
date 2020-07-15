a3902d5c63c4e039cd6d245c6d16ce95fa026cdb
mmanitta <intorobotics@gmail.com>
2020-07-15 13:38:46 +1000

    Update Task1.3C.ino

--- Module 1/Task1.3C/Task1.3C/Task1.3C.ino
+++ Module 1/Task1.3C/Task1.3C/Task1.3C.ino
@@ -3,7 +3,8 @@ volatile int soundState = 0;         // variable for reading the sound status
 
 void setup(){
   pinMode(13, OUTPUT);
- 
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
   //attach interupts to the ISR vectors
   attachInterrupt(0, pin_ISR, CHANGE);  // ISR on digital pin 2
   attachInterrupt(1, sound_ISR, CHANGE);  // ISR on digital pin 3
@@ -12,29 +13,29 @@ void setup(){
 }
 
 void loop(){
+  if (motionState && !soundState){
+    digitalWrite(13, HIGH);
+    Serial.println("Motion detected");
+  }
+  if (soundState && !motionState){
+    digitalWrite(13, HIGH);
+    Serial.println("Sound detected");
+  }
+  if (motionState && soundState){
+    digitalWrite(13, HIGH);
+    Serial.println("Motion and sound detected");
+  }
+  if (!motionState && !soundState){
+    digitalWrite(13, LOW);
+    Serial.println("No motion or sound detected");
+  }
  
 }
 
 void pin_ISR() {
   motionState = !motionState;
-  if (motionState){
-    digitalWrite(13, HIGH);
-    Serial.println("Motion detected");
-  }
-  else{
-    digitalWrite(13, LOW);
-    Serial.println("Motion ended");
-  }
 }
 
 void sound_ISR() {
   soundState = !soundState;
-  if (soundState){
-    digitalWrite(13, HIGH);
-    Serial.println("Sound detected");
-  }
-  else{
-    digitalWrite(13, LOW);
-    Serial.println("Sound ended");
-  }
 }
